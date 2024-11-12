aec8f2570597713493f1ddeea490238544daf469
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-11-12 14:08:52 +0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero (){
         <div className="bg-black w-full h-full">
             <div className="text-white font-serif font-bold flex justify-center items-center"> 
             <Image src="/Hina M.Rafiq.jpeg" alt="Profile Picture" height="280" width="220" className="mr-9 border border-solid border-black rounded-lg"></Image>
-                <h1 className="pt-28 text-3xl"><u>I'm Hina <br /> Muhammad Rafiq</u></h1>
+                <h1 className="pt-28 text-3xl"><u>I m Hina <br /> Muhammad Rafiq</u></h1>
             </div> 
         </div> 
     )
