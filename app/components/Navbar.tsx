d8f489320d0c5b678f7257f88c58cd0b37106de2
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-11-14 01:36:13 +0500

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,17 +9,17 @@ export default function Navbar(){
           <hr />
          <div id="nav-list" className="text-white flex justify-between items-center font-semibold tracking-wider ml-20  mt-4 w-4/5 pb-5">
             <Link href="/">
-               <div>Home
+               <div className="hover:underline">Home
                 </div></Link>
             <Link href="/About">
-                <div>About
+                <div className="hover:underline">About
                 </div></Link>
             <Link href="/Skills">
-                <div>Skills
+                <div className="hover:underline">Skills
                 </div>
             </Link>
             <Link href="/Contact">
-                <div>Contact
+                <div className="hover:underline">Contact
                 </div>
             </Link>
          </div>                
