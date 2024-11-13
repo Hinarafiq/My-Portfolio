15e1c2c8acbb6d0316f8593b54a50c6adcb2330e
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-11-14 01:37:42 +0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 export default function Hero (){
     return (
         <div className="bg-black w-full h-full">
-            <div className="text-white font-serif font-bold flex justify-center items-center"> 
+            <div className="text-white font-serif font-bold flex flex-col md:flex-row justify-center items-center"> 
             <Image src="/Hina M.Rafiq.jpeg" alt="Profile Picture" height="280" width="220" className="mr-9 border border-solid border-black rounded-lg"></Image>
                 <h1 className="pt-28 text-3xl"><u>I m Hina <br /> Muhammad Rafiq</u></h1>
             </div> 
