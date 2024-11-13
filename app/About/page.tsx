c7edf3e49a890a9e2c8adce47db1140bb9882843
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-11-14 01:56:36 +0500

    Update page.tsx

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,11 +1,11 @@
 export default function About (){
     return (
-      <div className=" bg-black text-white w-full h-96">
+      <div className=" bg-black text-white w-full h-full">
         <div className="flex items-center justify-center">
          <h1 className="font-semibold text-2xl mt-10"><u>About Me</u></h1> <br />
         </div> 
 
-        <div className="flex justify-center items-center font-light md:font-medium p-7 mt-12">
+        <div className="flex justify-center items-center font-light md:font-medium p-7 mt-7">
           <p>I m the undergraduate student after dedicated and results-driven professional with a proven track <br />
             record in the field of commerce. Passionate about exploring and embracing new technologies,<br />
              particularly in the world of GenAI. With a commitment to continuous learning and growth, <br />
