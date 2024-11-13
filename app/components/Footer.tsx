72bea49ee80c07e87300a4fdbf566a39cf50819b
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-11-14 01:38:25 +0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 export default function Footer() {
     return(
-        <div className="bg-black text-white flex justify-center items-center  w-full h-40 font-bold">
+        <div className="bg-black text-white flex justify-center items-center w-full h-40 pt-8 font-bold">
         <Link href="https://www.linkedin.com/in/hina-muhammad-rafiq-053b0a2b4" target="_blank">Made by <u>Hina Muhammad Rafiq</u></Link>
     </div>
     )
